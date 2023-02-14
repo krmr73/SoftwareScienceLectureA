f12fbda2e68dda51c96465487029356b34bb86bd
Nanami <88866882+krmr73@users.noreply.github.com>
2023-02-14 09:13:54 +0900

    変数代入、参照、連接を追加

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,4 @@
-# 抽象構文木
+# 式
 class Expr:
     def __init__(self, type) -> None:
         self.type = type
@@ -10,35 +10,35 @@ class BinExpr(Expr):
         self.lhs = lhs
         self.rhs = rhs
         
-    def evaluate(self):
+    def evaluate(self, env):
         match self.op:
             case '+' | '-' | '*' | '/':
-                return self.evaluateMathExpr()
+                return self.evaluateMathExpr(env)
             case '<' | '>' | '<=' | '>=' | '==' | '!=':
-                return self.evaluateCompExpr()
-            
-    def evaluateMathExpr(self):
+                return self.evaluateCompExpr(env)
+    # 四則演算        
+    def evaluateMathExpr(self, env):
         match self.op:
-            case '+': return self.lhs.evaluate() + self.rhs.evaluate()
-            case '-': return self.lhs.evaluate() - self.rhs.evaluate()
-            case '*': return self.lhs.evaluate() * self.rhs.evaluate()
-            case '/': return self.lhs.evaluate() / self.rhs.evaluate()
-            
-    def evaluateCompExpr(self):
+            case '+': return self.lhs.evaluate(env) + self.rhs.evaluate(env)
+            case '-': return self.lhs.evaluate(env) - self.rhs.evaluate(env)
+            case '*': return self.lhs.evaluate(env) * self.rhs.evaluate(env)
+            case '/': return self.lhs.evaluate(env) / self.rhs.evaluate(env)
+    # 比較        
+    def evaluateCompExpr(self, env):
         match self.op:
-            case '<':   return self.lhs.evaluate() < self.rhs.evaluate()
-            case '>':   return self.lhs.evaluate() > self.rhs.evaluate()
-            case '<=':  return self.lhs.evaluate() <= self.rhs.evaluate()
-            case '>=':  return self.lhs.evaluate() >= self.rhs.evaluate()
-            case '==':  return self.lhs.evaluate() == self.rhs.evaluate()
-            case '!=':  return self.lhs.evaluate() != self.rhs.evaluate()
+            case '<':   return self.lhs.evaluate(env) < self.rhs.evaluate(env)
+            case '>':   return self.lhs.evaluate(env) > self.rhs.evaluate(env)
+            case '<=':  return self.lhs.evaluate(env) <= self.rhs.evaluate(env)
+            case '>=':  return self.lhs.evaluate(env) >= self.rhs.evaluate(env)
+            case '==':  return self.lhs.evaluate(env) == self.rhs.evaluate(env)
+            case '!=':  return self.lhs.evaluate(env) != self.rhs.evaluate(env)
         
-
+# 整数
 class Int(Expr):
     def __init__(self, value) -> None:
         super().__init__("Int")
         self.value = value
-    def evaluate(self):
+    def evaluate(self, env):
         return self.value
 
 # 変数代入
@@ -47,19 +47,29 @@ class Assignment(Expr):
         super().__init__("Assignment")
         self.name = name
         self.expression = expression
+    def evaluate(self, env):
+        env[self.name] = self.expression.evaluate(env)
+        return env[self.name]
   
 # 変数参照      
 class Ident(Expr):
     def __init__(self, name) -> None:
         super().__init__("Ident")
         self.name = name
-    
+    def evaluate(self, env):
+        return env[self.name]
 # 連接    
 class Seq(Expr):
-    def __init__(self, bodies) -> None:
+    def __init__(self, *bodies) -> None:
         super().__init__("Seq")
         self.bodies = bodies
-        
+    def evaluate(self, env):
+        for b in self.bodies:
+            result = b.evaluate(env)
+        return result
+    
+
+           
 # 補助関数
 def tAdd(a, b):
     return BinExpr('+', a, b)
--- interpreter_test.py
+++ interpreter_test.py
@@ -4,68 +4,82 @@ from interpreter import *
 class TestArithmeticOperations(unittest.TestCase):
     def test_add1(self):
         e = tAdd(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), 2)
+        self.assertEqual(e.evaluate({}), 2)
         
     def test_add2(self):
         e = tAdd(Int(2), Int(3))
-        self.assertEqual(e.evaluate(), 5)
+        self.assertEqual(e.evaluate({}), 5)
         
     def test_sub1(self):
         e = tSub(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), 0)
+        self.assertEqual(e.evaluate({}), 0)
 
     def test_sub2(self):
         e = tSub(Int(1),Int(2))
-        self.assertEqual(e.evaluate(), -1)
+        self.assertEqual(e.evaluate({}), -1)
         
     def test_mul1(self):
         e = tMul(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), 1)
+        self.assertEqual(e.evaluate({}), 1)
 
     def test_mul2(self):
         e = tMul(Int(2),Int(2))
-        self.assertEqual(e.evaluate(), 4)
+        self.assertEqual(e.evaluate({}), 4)
         
     def test_mul3(self):
         e = tMul(Int(2),Int(0))
-        self.assertEqual(e.evaluate(), 0)
+        self.assertEqual(e.evaluate({}), 0)
         
     def test_div1(self):
         e = tDiv(Int(0), Int(1))
-        self.assertEqual(e.evaluate(), 0)
+        self.assertEqual(e.evaluate({}), 0)
 
     def test_div2(self):
         e = tDiv(Int(2),Int(1))
-        self.assertEqual(e.evaluate(), 2)
+        self.assertEqual(e.evaluate({}), 2)
         
     def test_div3(self):
         e = tDiv(Int(6),Int(2))
-        self.assertEqual(e.evaluate(), 3)
+        self.assertEqual(e.evaluate({}), 3)
         
 class TestComp(unittest.TestCase):
     def test_Lt(self):
         e = tLt(Int(1), Int(2))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
         
     def test_Gt(self):
         e = tGt(Int(2), Int(1))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
         
     def test_Lte(self):
         e = tLte(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
         
     def test_Gte(self):
         e = tGte(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
         
     def test_Eq(self):
         e = tEq(Int(1), Int(1))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
         
     def test_Neq(self):
         e = tNeq(Int(1), Int(2))
-        self.assertEqual(e.evaluate(), True)
+        self.assertEqual(e.evaluate({}), True)
+        
+class TestSeqAssign(unittest.TestCase):
+    def test_seq_assign1(self):
+        e = Seq(Assignment('a', Int(100)), Ident('a'))
+        self.assertEqual(e.evaluate({}), 100)
+        
+    def test_seq_assign2(self):
+        e = Seq(
+            Assignment('a', Int(100)),
+            Assignment('b', tAdd(Ident('a'), Int(1))),
+            Ident('b')
+            )
+        self.assertEqual(e.evaluate({}), 101)
+        
 
 if __name__ == '__main__':
     unittest.main()
